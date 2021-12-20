c08e31f6d154a4461394b897a0543b91bda1394f
Anna Luiza Gomes <38672763+annalug@users.noreply.github.com>
2021-12-20 18:26:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 st.title('Clustering Sci-Fi movies by scripts using Topic Modeling with LDA (Latent Dirichlet Allocation)')
 
+st.write('The main goal of this project is to clusterize Sci-Fi movies by its scripts, defining a dominant topic for each movie (script) in order to form groups alike. A next step would be to be able to infer the topics and thus classify new movies (scripts).')
 st.write('The scripts ara available on https://imsdb.com/genre/Sci-Fi')
 st.write('It was divided in 3 topics shown below:')
 st.image('.//assets//topic_distrib.png')
